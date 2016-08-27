f980b7b63fdc136ccec2f05c91d49921156b5b4a
birdy90 <go.birdy.90@gmail.com>
2016-08-27 13:18:48 +0300

    * fast fixes for player

--- Assets/Scripts/PlayerMoving.cs
+++ Assets/Scripts/PlayerMoving.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class PlayerMoving : MonoBehaviour {
 
+    public float SpeedVelue = 0.1f;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,8 +12,9 @@ public class PlayerMoving : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        var x = Input.GetAxis("Horizontal") * 0.1f;
-        var z = Input.GetAxis("Vertical") * 0.1f;
-        transform.Translate(x, 0f, z, Space.World);
+        var x = Input.GetAxis("Horizontal") * SpeedVelue;
+        var z = Input.GetAxis("Vertical") * SpeedVelue;
+        var speedVector = new Vector3(x, 0f, z);
+        transform.Translate(speedVector.normalized, Space.World);
     }
 }
