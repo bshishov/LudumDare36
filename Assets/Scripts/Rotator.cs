1ee296e88b4fb8f544d076d7e8813c55eb2f2fbb
birdy90 <go.birdy.90@gmail.com>
2016-08-28 18:26:22 +0300

    + rotator

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+    public float RotationSpeed = 90f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.Rotate(0f, RotationSpeed * Time.deltaTime, 0f, Space.World);
+	}
+}
