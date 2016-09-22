fffe3af052cf36b5fed070834757d63b017d8f08
Boris shishov <borisshishov@gmail.com>
2016-09-22 17:52:41 +0300

    New animated rotation mechanic

---
+++ Assets/Scripts/Gameplay/AnimatedRotate.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatedRotate : MonoBehaviour
+{
+    public Vector3 RotationPoint;
+    public Vector3 RotationAxis;
+    public AnimationCurve Curve;
+    public float Offset;
+
+    [Range(0.1f, 20f)]
+    public float Speed = 1f;
+
+    [Range(0.1f, 360f)]
+    public float Amount = 1f;
+
+    private Quaternion _initialRotation;
+    private Vector3 _initialPosition;
+    private Vector3 _pivot;
+    private Vector3 _rotationAxis;
+
+    void Start ()
+    {
+        _initialRotation = transform.rotation;
+        _initialPosition = transform.position;
+        _pivot = transform.TransformPoint(RotationPoint);
+        _rotationAxis = transform.TransformVector(RotationAxis);
+    }
+	
+	void Update ()
+	{
+	    var rot = Quaternion.AngleAxis(Curve.Evaluate(Time.time * Speed) * Amount, _rotationAxis);
+        var rel = _initialPosition - _pivot;
+	    rel = rot * rel;
+        transform.position = _initialPosition + rel - (_initialPosition - _pivot);
+        transform.rotation = rot * _initialRotation;
+	}
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.TransformPoint(RotationPoint), 0.5f);
+        
+        Gizmos.DrawLine(transform.TransformPoint(RotationPoint), transform.TransformPoint(RotationPoint) + _rotationAxis);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(_pivot, 0.5f);
+    }
+}
