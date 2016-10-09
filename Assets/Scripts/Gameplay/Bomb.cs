e0b3b50ef449be7a73dded7d28b39466b9b77597
Boris shishov <borisshishov@gmail.com>
2016-10-09 20:09:36 +0300

    Bomb throwing

---
+++ Assets/Scripts/Gameplay/Bomb.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(Collider))]
+public class Bomb : NetworkBehaviour
+{
+    public GameObject DestroyEffectPrefab;
+	
+	void Start ()
+    {
+	}	
+	
+	void Update ()
+    {
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (DestroyEffectPrefab != null)
+        {
+            GameObject.Instantiate(DestroyEffectPrefab, transform.position, Quaternion.identity);
+        }
+
+        if (collision.gameObject.CompareTag(Tags.Player))
+        {
+            Debug.Log("Bomb collision with player");
+        }
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Gameplay/PlayerBombThrower.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerBombThrower : NetworkBehaviour
+{
+    public GameObject BombPrefab;
+    public Vector3 Force = new Vector3(0, 0.1f, 1f);
+    public Vector3 Offset = new Vector3();
+    public float StartRadius;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Throw();
+        }
+    }
+
+    void Throw()
+    {
+        if (BombPrefab == null)
+        {
+            Debug.LogWarning("Bomb prefab is empty");
+            return;
+        }
+
+        var direction = transform.TransformDirection(Vector3.forward);
+        var bomb = GameObject.Instantiate(BombPrefab, transform.position + direction * StartRadius + Offset, Quaternion.identity) as GameObject;
+        var rigidBody = bomb.GetComponent<Rigidbody>();
+        if (rigidBody != null)
+        {
+            rigidBody.AddForce(transform.TransformVector(Force), ForceMode.Impulse);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position + Offset, StartRadius);
+    }
+}
